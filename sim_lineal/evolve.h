891db6e133a1c096c543c3dd614c230a62530890
David Bernal <bernalde@hotmail.com>
2016-05-12 16:40:41 -0500

    Corri una en suma directa para comparar

--- sim_lineal/evolve.h
+++
@@ -1,6 +0,0 @@
-double calcula_tiempo_total(int n);
-double calcula_time_step(int n, double epsilon);
-void calcula_aceleracion(double *p, double *v, double *a, int n, double epsilon);
-void  kick(double *p, double *v, double *a, int n, double delta_t);
-void  drift(double *p, double *v, double *a, int n, double delta_t);
-void calcula_energia(double *p, double *v, double *U, double *K, int n);
--- temp/inicial.h
+++
@@ -1,4 +0,0 @@
-void escribe_estado(double *x, double *y, double *U, double *K, int n, int id);
-void posiciones_iniciales(double *p, int n);
-double *crea_vector(int n);
-void recibe_input(int argc, char **argv, int *n, double *e, int *th);
